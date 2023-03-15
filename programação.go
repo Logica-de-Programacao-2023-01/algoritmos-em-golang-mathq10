5882aa98eeac84d5a06c4e4816b218c1437b5b1a
mathq10 <126472948+mathq10@users.noreply.github.com>
2023-03-15 16:29:45 -0200

    Add files via upload

---
+++ programação.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var idade int
+	x := 365
+	fmt.Println("digite sua idade em anos:")
+	fmt.Scan(&idade)
+	idade_em_dias := idade * x
+	fmt.Printf("sua idade em dias é: %d", idade_em_dias)
+}

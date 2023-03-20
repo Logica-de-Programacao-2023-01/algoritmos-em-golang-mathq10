e55dd075d03fed8b090f216969461c881e519e7b
mathq10 <126472948+mathq10@users.noreply.github.com>
2023-03-20 09:50:43 -0300

    Add files via upload

--- CC.go
+++ CC.go
@@ -1,23 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var num1, num2, num3 int
+	var num1 int
 	fmt.Println("digite um número:")
 	fmt.Scan(&num1)
-	fmt.Println("digite o segundo número:")
-	fmt.Scan(&num2)
-	fmt.Println("digite o terceiro numero:")
-	fmt.Scan(&num3)
-	if num1 < num2 && num1 < num3 {
-		fmt.Printf("%d é menor que todos os outros números acima", num1)
-	} else if num2 < num1 && num2 < num3 {
-		fmt.Printf("%d é menor que todos os outros números acima", num2)
-	} else if num3 < num1 && num3 < num2 {
-		fmt.Printf("%d é menor que todos os outros números acima", num3)
+	if num1%2 == 0 {
+		fmt.Printf("O número é par")
 	} else {
-		fmt.Printf("todos os números são iguais")
+		fmt.Printf("o numero é impar")
 	}
 
 }

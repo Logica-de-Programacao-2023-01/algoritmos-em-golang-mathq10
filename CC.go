2849a9651e63883faf937fac272e2ddfa6d38333
mathq10 <126472948+mathq10@users.noreply.github.com>
2023-03-20 10:49:50 -0300

    Add files via upload

---
+++ CC.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var altura, peso, imc float64
+	var sexo string
+	fmt.Println("qual sua altura?")
+	fmt.Scan(&altura)
+	fmt.Println("qual seu sexo?")
+	fmt.Scan(&sexo)
+	fmt.Println("qual seu peso?")
+	fmt.Scan(&peso)
+	imc := peso / (altura * altura)
+	if sexo == "homem" {
+		if imc < 18
+			fmt.Printf("você esta abaixo do peso ideal")
+	} else if imc >= 18 && imc <= 25 {
+		fmt.Printf("você esta no peso ideal, parabens")
+	} else {
+		fmt.Printf("você esta acima do peso")
+	} else if sexo == "mulher" {
+		if imc < 18 {
+			fmt.Printf("você esta abaixo do peso ideal")
+		} else if imc >= 18 && imc <= 24
+			fmt.Printf("você esta no peso ideal, parabens")
+	} else {
+		fmt.Printf("você esta acima do peso ideal")
+	}
+}
+
+
+
+}
